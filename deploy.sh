2106ce74f255da9e0dd34430fdd1b8aa621987c8
mdalaminbey <75071900+mdalaminbey@users.noreply.github.com>
2025-11-15 06:25:16 +0600

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -140,7 +140,7 @@ svn update
 svn status
 
 # Commit changes
-# echo "➤ Committing files..."
-# svn commit -m "Updating readme/assets from GitHub" --no-auth-cache --non-interactive --username "$SVN_USERNAME" --password "$SVN_PASSWORD"
+echo "➤ Committing files..."
+svn commit -m "Updating readme/assets from GitHub" --no-auth-cache --non-interactive --username "$SVN_USERNAME" --password "$SVN_PASSWORD"
 
-# echo "✓ Plugin assets and readme updated (including tag)!"
+echo "✓ Plugin assets and readme updated (including tag)!"
